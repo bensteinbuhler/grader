919c6861465677ed00cce337ba70c75a8f136973
Ubuntu <ubuntu@ip-172-31-36-116.us-west-2.compute.internal>
2013-07-09 23:31:56 +0000

    Fixed command line problem.

--- grader.js
+++ grader.js
@@ -29,7 +29,7 @@ var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
+    if(!fs.readFileSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
